1072ea184b28a461437314a4f4fce2fe40345f91
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-12-14 01:29:47 -0800

    error removal

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #define _GNU_SOURCE
 
-/*bus_t bus = {NULL, NULL, NULL, 0};*/
+bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
  * main - function for monty code interpreter
@@ -14,7 +14,6 @@
 
 int main(int argc, char *argv[])
 {
-	bus_t bus = {NULL, NULL, NULL, 0};
 	char *content;
 	FILE *file;
 	size_t size = 0;
