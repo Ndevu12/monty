2f2152663be71a62bd376d0986ca2732409c32e5
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-12-13 23:18:17 -0800

    stack

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define _GNU_SOURCE
+
+bus_T bus = {NULL, NULL, NULL, 0};
+
+/**
+ * main - function for monty code interpreter
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	cha r*content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_T *stack = NULL;
+	unsigned int counter = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		couter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+	return (0);
+}
+
